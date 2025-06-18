1149c96dc6457c726ad3b7245d931b5962e5bee4
Sean Murphy <sean@iamseanmurphy.com>
2025-06-18 07:17:48 +0000

    1.1.2

--- animated-text-block/build/index.asset.php
+++ animated-text-block/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => 'a441e805e106c27e8964');
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-i18n'), 'version' => '8706694f00aafa0998aa');
--- animated-text-block/build/view.asset.php
+++ animated-text-block/build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '2771faae771580912194');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '6cb13b3a0b4ce30d832c');
